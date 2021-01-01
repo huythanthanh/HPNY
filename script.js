a4d336106a5757518da27b340893f897f452816c
Trum Huy <54362170+huythanthanh@users.noreply.github.com>
2021-01-01 21:01:55 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -32,29 +32,29 @@ $(document).ready(function () {
 }); //doc.ready()
 
 
-var x = document.getElementById("myDIV").addEventListener("wheel", myFunction);
+// var x = document.getElementById("myDIV").addEventListener("wheel", myFunction);
 
-function myFunction() {
-  $scope.sound = function () {
-    if ($scope.totalQueueList) {
-        var audio = new Audio();
-        audio.src = 'nhacsieuhay.mp3';
-        // when the sound has been loaded, execute your code
-        audio.oncanplaythrough = (event) => {
-            var playedPromise = audio.play();
-            if (playedPromise) {
-                playedPromise.catch((e) => {
-                    console.log(e)
-                    if (e.name === 'NotAllowedError' || e.name === 'NotSupportedError') {
-                        console.log(e.name);
-                    }
-                }).then(() => {
+// function myFunction() {
+//   $scope.sound = function () {
+//     if ($scope.totalQueueList) {
+//         var audio = new Audio();
+//         audio.src = 'nhacsieuhay.mp3';
+//         // when the sound has been loaded, execute your code
+//         audio.oncanplaythrough = (event) => {
+//             var playedPromise = audio.play();
+//             if (playedPromise) {
+//                 playedPromise.catch((e) => {
+//                     console.log(e)
+//                     if (e.name === 'NotAllowedError' || e.name === 'NotSupportedError') {
+//                         console.log(e.name);
+//                     }
+//                 }).then(() => {
 
-                });
-            }
-        }
-    }
-}
+//                 });
+//             }
+//         }
+//     }
+// }
 
-}
+// }
 
